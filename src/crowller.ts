05c135519c5db791a4363c37ef8c0eb2d1fec305
Bill78Zhang <wohencool21@gmail.com>
2020-10-09 15:18:40 +1030

    write json file

--- src/crowller.ts
+++ src/crowller.ts
@@ -1,14 +1,27 @@
+import fs from 'fs';
+import path from 'path';
 import superagent from 'superagent';
 import cheerio from 'cheerio';
+
 interface Movie {
   title: string;
   rating?: number;
+}
+
+interface MovieInfo {
+  time: Date;
+  data: Movie[];
+}
+
+interface Content {
+  [propsName: number]: Movie[];
 }
 
 class Crowller {
   private url: string = 'https://www.imdb.com/chart/top/';
 
   movieInfo: Movie[] = [];
+  movieCollection: MovieInfo[] = [];
 
   getMovieInfo(html: string) {
     const $ = cheerio.load(html);
@@ -40,12 +53,25 @@ class Crowller {
         });
       }
     });
-    console.log(this.movieInfo);
+    const movieResult: MovieInfo = { time: new Date(), data: this.movieInfo };
+
+    this.generateJsonContent(movieResult);
   }
 
   async getHtml() {
     const results = await superagent(this.url);
     this.getMovieInfo(results.text);
+  }
+
+  generateJsonContent(info: MovieInfo) {
+    const filePath = path.resolve(__dirname, '../data/movie.json');
+    let content: Content = {};
+    if (fs.existsSync(filePath)) {
+      content = JSON.parse(fs.readFileSync(filePath, 'utf-8'));
+    }
+    const date = Number(info.time);
+    content[date] = info.data;
+    fs.writeFileSync(filePath, JSON.stringify(content));
   }
 
   constructor() {
