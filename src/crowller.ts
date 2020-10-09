dae20810156e574fe23f19e26bf182aeb870fd18
Bill78Zhang <wohencool21@gmail.com>
2020-10-09 14:33:32 +1030

    get data

--- src/crowller.ts
+++ src/crowller.ts
@@ -1,17 +1,51 @@
 import superagent from 'superagent';
-
+import cheerio from 'cheerio';
 interface Movie {
-  name: string;
-  rating: number;
+  title: string;
+  rating?: number;
 }
 
 class Crowller {
-  private url: string = 'https://www.imdb.com/chart/top';
-  private rawHtml = '';
+  private url: string = 'https://www.imdb.com/chart/top/';
+
+  movieInfo: Movie[] = [];
+
+  getMovieInfo(html: string) {
+    const $ = cheerio.load(html);
+    const MovieTitle = $('.titleColumn');
+
+    MovieTitle.map((index, item) => {
+      if (index < 20) {
+        const title = $(item)
+          .find('a')
+          .text();
+        this.movieInfo.push({ title });
+      }
+    });
+    const MovieRating = $('.imdbRating');
+    MovieRating.map((index, item) => {
+      if (index < 20) {
+        const rating: number = parseFloat(
+          $(item)
+            .find('strong')
+            .text()
+        );
+        const order = index;
+        this.movieInfo = this.movieInfo.map((item, index) => {
+          if (index === order) {
+            return { rating, ...item };
+          } else {
+            return item;
+          }
+        });
+      }
+    });
+    console.log(this.movieInfo);
+  }
 
   async getHtml() {
     const results = await superagent(this.url);
-    this.rawHtml = results.text;
+    this.getMovieInfo(results.text);
   }
 
   constructor() {
